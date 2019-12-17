d8b09af1717c05ea0fb7a7072cf9104a29cc4d4c
704903177 <h_yunxiao123@163.com>
2019-12-17 10:01:13 +0800

    user add

--- src/main/java/com/example/jenkins2/Jenkins2Application.java
+++ src/main/java/com/example/jenkins2/Jenkins2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Jenkins2Application {
 
     public static void main(String[] args) {
+        System.out.println("test");
         SpringApplication.run(Jenkins2Application.class, args);
     }
 
